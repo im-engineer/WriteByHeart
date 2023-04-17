c8cb7a7e25f0142e293d65740369b8c63a9659d2
im-engineer <azmsiddhant1@gmail.com>
2023-04-17 12:55:14 +0530

    changes in app

--- serverside/app.js
+++ serverside/app.js
@@ -2,12 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 const app = express();
 
-// import cors from 'cors'
+import cors from 'cors'
 import {mongoconnection} from './db';
 mongoconnection();
 import writerRoute from "./routes/writerRoute/writerRoute"
 import adminRoute from "./routes/adminRoute/adminRoute"
-// app.use(cors({origin:"*"}));
+app.use(cors({origin:"*"}));
 
 app.use(bodyParser.urlencoded(
     {
