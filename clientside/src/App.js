0ff112c697348d5111b6e9b4e396ad27d52d3639
im-engineer <azmsiddhant1@gmail.com>
2023-04-26 12:08:48 +0530

    create profile page and assign path in app

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Main from './pages/main/Main'
 import Publish from './component/Publish/Publish';
 import LandingPage from './component/Landing/LandingPage';
+import Profile from './component/Profile/Profile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Main/>}/>
         <Route path="/landingPage" element={<LandingPage/>}/>
         <Route path="/publish" exact element={<Publish/>} />
+        <Route path='profile' element={<Profile/>}/>
       </Routes>
     </div>
   );
---
+++ clientside/src/component/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Profile.css"
+
+function Profile() {
+  return (
+    <div className='profile--page'>Profile</div>
+  )
+}
+
+export default Profile
