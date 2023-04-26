a1ac60b82418c34972e51b9ac41039aa9ee8cf52
im-engineer <azmsiddhant1@gmail.com>
2023-04-26 17:53:39 +0530

    change in latest poetry

--- clientside/src/component/Latest/LatestPoetry.js
+++ clientside/src/component/Latest/LatestPoetry.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LatestPoetry.css";
 import { useSelector } from "react-redux";
 import { Card } from "react-bootstrap";
@@ -10,6 +10,18 @@ function LatestPoetry() {
     userdata
   );
   const poetry = userdata.data.result.poetry;
+  const [currentPage, setCurrentPage] = useState(1);
+  const poemsPerPage = 6;
+
+  // Logic for displaying poems
+  const indexOfLastPoem = currentPage * poemsPerPage;
+  const indexOfFirstPoem = indexOfLastPoem - poemsPerPage;
+  const currentPoems = poetry.slice(indexOfFirstPoem, indexOfLastPoem);
+
+  // Logic for updating current page
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <>
@@ -18,20 +30,34 @@ function LatestPoetry() {
           Recently Added Poetry
         </h3>
         <div className="row d-flex justify-content-center align-items-center">
-        {poetry.map((poem) => (
-          <Card
-            className="card_style mx-2  "
-          >
-            {/* <Card.Body> */}
-              <div className="card" key={poem.id}>
+          {currentPoems.map((poem) => (
+            <Card className="card_style mx-2" key={poem.id}>
+              <div className="card">
                 <Card.Text>{poem.title}</Card.Text>
                 <Card.Text>{poem.content}</Card.Text>
                 <Card.Text>{poem.author}</Card.Text>
                 <Card.Text>{poem.genre}</Card.Text>
               </div>
-            {/* </Card.Body> */}
-          </Card>
-        ))}
+            </Card>
+          ))}
+        </div>
+        <div className="pagination">
+          <ul className="pagination-list">
+            {poetry.length > poemsPerPage &&
+              Array(Math.ceil(poetry.length / poemsPerPage))
+                .fill(null)
+                .map((_, index) => (
+                  <li
+                    key={index + 1}
+                    className={`pagination-item ${
+                      currentPage === index + 1 ? "active" : ""
+                    }`}
+                    onClick={() => handlePageChange(index + 1)}
+                  >
+                    {index + 1}
+                  </li>
+                ))}
+          </ul>
         </div>
       </div>
     </>
