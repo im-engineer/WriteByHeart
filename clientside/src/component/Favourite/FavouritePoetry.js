a43f4e4a4aedb84bad69e3c04be8f6d0be74ae1a
im-engineer <azmsiddhant1@gmail.com>
2023-04-26 15:14:45 +0530

    changes update

--- clientside/src/component/Favourite/FavouritePoetry.js
+++ clientside/src/component/Favourite/FavouritePoetry.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './FavouritePoetry.css'
 function FavouritePoetry() {
   return (
-    <div className='favouritepoetry--page'>FavouritePoetry</div>
+    <div className='favouritepoetry--page'>
+            <h3 style={{color:"white", float:"right", padding:"1rem"}}>Most Liked Poetry</h3>
+    </div>
   )
 }
 
--- clientside/src/component/Latest/LatestPoetry.js
+++ clientside/src/component/Latest/LatestPoetry.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './LatestPoetry.css'
 function LatestPoetry() {
   return (
-    <div className='latestpoetry--page'>LatestPoetry</div>
+    <div className='latestpoetry--page'>
+      <h3 style={{color:"white", float:"left", padding:"1rem"}}>Recently Added Poetry</h3>
+    </div>
   )
 }
 
