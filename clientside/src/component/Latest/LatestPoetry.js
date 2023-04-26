f42bb35b00d7b4aa2f9c5f2d7a12159b444c8287
im-engineer <azmsiddhant1@gmail.com>
2023-04-26 17:43:00 +0530

    change in latest poetry

--- clientside/src/component/Latest/LatestPoetry.js
+++ clientside/src/component/Latest/LatestPoetry.js
@@ -3,7 +3,6 @@ import "./LatestPoetry.css";
 import { useSelector } from "react-redux";
 import { Card } from "react-bootstrap";
 
-
 function LatestPoetry() {
   const userdata = useSelector((state) => state.auth);
   console.log(
@@ -18,17 +17,27 @@ function LatestPoetry() {
         <h3 style={{ color: "white", float: "left", padding: "1rem" }}>
           Recently Added Poetry
         </h3>
-        <Card >
-          <Card.Body>
-          {poetry.map((poem) => (
-            <div className="card" key={poem.id}>
-              <Card.Title>{poem.title}</Card.Title>
-              <Card.Title>{poem.content}</Card.Title>
-              {/* Render other details of the poem as needed */}
-            </div>
-          ))}
-          </Card.Body>
+        <div className="row d-flex justify-content-center align-items-center">
+        {poetry.map((poem) => (
+          <Card
+            style={{
+              width: "50%",
+              border: "none",
+              marginBottom: "2rem",
+              height: "10rem",
+            }}
+            className="card_style mx-2 mt-4 "
+          >
+            {/* <Card.Body> */}
+              <div className="card" key={poem.id}>
+                <Card.Title>{poem.title}</Card.Title>
+                <Card.Title>{poem.content}</Card.Title>
+                {/* Render other details of the poem as needed */}
+              </div>
+            {/* </Card.Body> */}
           </Card>
+        ))}
+        </div>
       </div>
     </>
   );
