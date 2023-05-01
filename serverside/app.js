0da26a8102193a669571b9210f7cad322d7f1ac7
im-engineer <azmsiddhant1@gmail.com>
2023-05-01 17:41:57 +0530

    changes in app

--- serverside/app.js
+++ serverside/app.js
@@ -17,6 +17,6 @@ app.use(bodyParser.json());
 
 app.use("/admin",adminRoute)
 app.use("/writer",writerRoute)
-app.use("/uploads",express.static("./uploads"));
+app.use("/uploads",express.static("Image"));
 
 export default app;
