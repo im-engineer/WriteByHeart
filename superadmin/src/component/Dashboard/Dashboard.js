cf85594daf564cc0a56dcbbc46deadb165bbaee5
im-engineer <azmsiddhant1@gmail.com>
2023-05-01 18:28:44 +0530

    changes in dashboard

--- superadmin/src/component/Dashboard/Dashboard.js
+++ superadmin/src/component/Dashboard/Dashboard.js
@@ -1,29 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { totalWriters, totalactiveinactivewriters } from "../../service/authService";
+import {
+  totalWriters,
+  totalactiveinactivewriters,
+} from "../../service/authService";
 import "./Dashboard.css";
 
 function Dashboard() {
-  const [show, setShow] = useState(" ");
+  const [show, setShow] = useState("");
 
   useEffect(() => {
     const count = async () => {
-      const result = await totalWriters();
-      const TotalWriters = result.data.totalWriters;
-      setShow(TotalWriters);
+      try {
+        const result = await totalWriters();
+        const TotalWriters = result.data.totalWriters;
+        setShow(TotalWriters);
+      } catch (error) {
+        // Handle error
+        console.log(`error : ${error}`);
+      }
     };
     count();
-  });
+  }, []); // Add an empty dependency array to trigger the effect only on mount
 
-  const[status, setStatus] = useState(" ");
+  const [status, setStatus] = useState([]);
 
   useEffect(() => {
     const writerStatus = async () => {
-        const result = await totalactiveinactivewriters();
-        setStatus(result)
-    }
-    writerStatus()
-  })
-
+      const result = await totalactiveinactivewriters();
+      const allStatus = result.data;
+      console.log(
+        "🚀 ~ file: Dashboard.js:31 ~ writerStatus ~ allStatus:",
+        allStatus
+      );
+      setStatus(allStatus);
+    };
+    writerStatus();
+  }, []); // Add an empty dependency array to trigger the effect only on mount
+console.log(status)
   return (
     <div class="container">
       <div class="row">
@@ -32,26 +45,25 @@ function Dashboard() {
             <div class="card-block">
               <h6 class="m-b-20">Total Writers</h6>
               <h2 class="text-right">
-                <i class="fa fa-users  f-left"></i>
+                <i class="fa fa-users f-left"></i>
                 <span>{show}</span>
               </h2>
               <p class="m-b-0">
-               Total<span class="f-right">{show} witers joined</span>
+                Total<span class="f-right">{show} witers joined</span>
               </p>
             </div>
           </div>
         </div>
-
         <div class="col-md-4 col-xl-3">
           <div class="card bg-c-green order-card">
             <div class="card-block">
               <h6 class="m-b-20">Active Writers</h6>
               <h2 class="text-right">
                 <i class="fa fa-user-check f-left"></i>
-                <span>{status.data.activeWritersCount}</span>
+                <span>{status.activeWritersCount}</span>
               </h2>
               <p class="m-b-0">
-                Total<span class="f-right">{status.data.activeWritersCount}  writer active</span>
+                <span class="f-right">{status.activeWritersCount} writer active</span>
               </p>
             </div>
           </div>
@@ -60,13 +72,13 @@ function Dashboard() {
         <div class="col-md-4 col-xl-3">
           <div class="card bg-c-yellow order-card">
             <div class="card-block">
-              <h6 class="m-b-20">Inactive writer</h6>
+              <h6 class="m-b-20">Inactive Writer</h6>
               <h2 class="text-right">
-                <i class="fa fa-users-slash f-left"></i>
-                <span>{status.data.inactiveWritersCount}</span>
+                <i class="fa fa-user-slash f-left"></i>
+                <span>{status.inactiveWritersCount}</span>
               </h2>
               <p class="m-b-0">
-                Total <span class="f-right">{status.data.inactiveWritersCount} are not active</span>
+                <span class="f-right">{status.inactiveWritersCount} writer not active</span>
               </p>
             </div>
           </div>
