2d6b869d293229b76466ce4daac07c7668efc1de
im-engineer <azmsiddhant1@gmail.com>
2023-05-01 17:42:38 +0530

    changes in latest poetry

--- clientside/src/component/Latest/LatestPoetry.js
+++ clientside/src/component/Latest/LatestPoetry.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./LatestPoetry.css";
 import { Card } from "react-bootstrap";
 import { getAllPoetry } from "../../service/authService";
+import img from "../../assets/jpeg/logo.png";
 
 function LatestPoetry() {
   const [poetry, setPoetry] = useState([]);
@@ -10,8 +11,11 @@ function LatestPoetry() {
   useEffect(() => {
     const fetchPoetry = async () => {
       const result = await getAllPoetry();
-      const allPoetry = result.data
-      console.log("🚀 ~ file: LatestPoetry.js:14 ~ fetchPoetry ~ allPoetry:", allPoetry)
+      const allPoetry = result.data;
+      console.log(
+        "🚀 ~ file: LatestPoetry.js:14 ~ fetchPoetry ~ allPoetry:",
+        allPoetry
+      );
       setPoetry(allPoetry);
     };
 
@@ -30,22 +34,47 @@ function LatestPoetry() {
   // const startIndex = (currentPage - 1) * itemsPerPage;
   // const endIndex = startIndex + itemsPerPage;
 
-
   return (
     <div className="latestpoetry--page">
       <h3 style={{ color: "white", padding: "1rem" }}>Recently Added Poetry</h3>
       <div className="row d-flex justify-content-center align-items-center">
-        {poetry.map((poem) => (
-          <Card className="mx-1" key={poem.id} style={{ backgroundImage: `url(${poem.image})`,backgroundSize:"cover", backgroundPosition:"center center" }}>
-            <div className="card">
-              <Card.Text>{poem.title}</Card.Text>
-              <Card.Text>{poem.author}</Card.Text>
-              <Card.Text>{poem.content}</Card.Text>
-              <Card.Text>{poem.genre}</Card.Text>
-            </div>
-          </Card>
-        ))}
+        {poetry.map((poem) => {
+          let path = `http://localhost:5001/upload/${poem.image}`;
+          console.log(
+            "🚀 ~ file: LatestPoetry.js:44 ~ LatestPoetry ~ path:",
+            path
+          );
+
+          return (
+            <Card
+              className="mx-2"
+              key={poem.id}
+            >
+              <div
+                className="card"
+                img={path}
+                style={{
+                  color: "red",
+                  backgroundImage: `url(${path})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  height: "100%",
+                  width: "100%",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>{poem.title}</Card.Title>
+                  <Card.Subtitle>{poem.author}</Card.Subtitle>
+                  <Card.Text>{poem.content}</Card.Text>
+                  <Card.Text>{poem.genre}</Card.Text>
+                </Card.Body>
+              </div>
+            </Card>
+          );
+        })}
       </div>
+
       {/* <div className="pagination-buttons">
       <button
               disabled={currentPage === 1}
