ee8b38b7fdb823a79bf8cd25e77fd1960cc9b9cd
im-engineer <azmsiddhant1@gmail.com>
2023-11-22 18:10:09 +0530

    changes to show image

--- clientside/src/component/Latest/LatestPoetry.js
+++ clientside/src/component/Latest/LatestPoetry.js
@@ -6,94 +6,53 @@ import img from "../../assets/jpeg/logo.png";
 
 function LatestPoetry() {
   const [poetry, setPoetry] = useState([]);
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const itemsPerPage = 10;
+
   useEffect(() => {
     const fetchPoetry = async () => {
-      const result = await getAllPoetry();
-      const allPoetry = result.data;
-      console.log(
-        "🚀 ~ file: LatestPoetry.js:14 ~ fetchPoetry ~ allPoetry:",
-        allPoetry
-      );
-      setPoetry(allPoetry);
+      try {
+        const result = await getAllPoetry();
+        const allPoetry = result.data;
+        console.log("All Poetry:", allPoetry);
+        setPoetry(allPoetry);
+      } catch (error) {
+        console.error("Error fetching poetry:", error);
+      }
     };
 
     fetchPoetry();
   }, []);
 
-  // // Calculate total number of pages
-  // const totalPages = Math.ceil(poetry.length / itemsPerPage);
-
-  // // Update current page
-  // const handlePageChange = (newPage) => {
-  //   setCurrentPage(newPage);
-  // };
-
-  // // Calculate start and end index for current page
-  // const startIndex = (currentPage - 1) * itemsPerPage;
-  // const endIndex = startIndex + itemsPerPage;
-
   return (
     <div className="latestpoetry--page">
       <h3 style={{ color: "white", padding: "1rem" }}>Recently Added Poetry</h3>
       <div className="row d-flex justify-content-center align-items-center">
-        {poetry.map((poem) => {
-          let path = `http://localhost:5001/upload/${poem.image}`;
-          console.log(
-            "🚀 ~ file: LatestPoetry.js:44 ~ LatestPoetry ~ path:",
-            path
-          );
-
-          return (
-            <Card
-              className="mx-2"
-              key={poem.id}
-            >
-              <div
-                className="card"
-                img={path}
-                style={{
-                  color: "red",
-                  backgroundImage: `url(${path})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  height: "100%",
-                  width: "100%",
-                }}
-              >
+        {poetry.length === 0 ? (
+          <p style={{ color: "white" }}>No poetry data available</p>
+        ) : (
+          poetry.map((poem) => {
+            let path = `http://localhost:5001/upload/${poem.image}`;
+            console.log("Image Path:", path);
+            return (
+              <Card className="mx-2" key={poem.id}>
+                <Card.Img
+                  variant="top"
+                  src={path}
+                  style={{ height: "25rem", marginTop: "1rem" }}
+                  onError={(e) => {
+                    console.error("Image failed to load:", e);
+                  }}
+                />
                 <Card.Body>
                   <Card.Title>{poem.title}</Card.Title>
                   <Card.Subtitle>{poem.author}</Card.Subtitle>
                   <Card.Text>{poem.content}</Card.Text>
                   <Card.Text>{poem.genre}</Card.Text>
                 </Card.Body>
-              </div>
-            </Card>
-          );
-        })}
+              </Card>
+            );
+          })
+        )}
       </div>
-
-      {/* <div className="pagination-buttons">
-      <button
-              disabled={currentPage === 1}
-              onClick={() => handlePageChange(currentPage - 1)}
-              className="pagination-button"
-            >
-              <i class="fa fa-angle-double-left" aria-hidden="true"></i>
-            </button>
-            <span>
-              Page {currentPage} of {totalPages}
-            </span>
-            <button
-              disabled={currentPage === totalPages}
-              onClick={() => handlePageChange(currentPage + 1)}
-              className="pagination-button"
-            >
-              <i class="fa fa-angle-double-right"  aria-hidden="true"></i>
-            </button>
-      </div> */}
     </div>
   );
 }
